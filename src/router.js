9ec52c556da0e7a0238c6d4f532881a0945e52f0
Mendev200 <irca.mendev@gmail.com>
2024-04-13 09:06:51 +0200

    adding paths and functions

--- src/router.js
+++ src/router.js
@@ -1,14 +1,28 @@
+import React from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import AddContact from './pages/AddContact';
+import Header from './pages/Header';
+import Home from './pages/Home';
+import ShowUser from './pages/ShowUser';
+import SignIn from './pages/SignIn';
+import SignUp from './pages/SignUp';
+import UpdateUser from './pages/UpdateUser';
 
 
 function AppRouter() {
-    return(
-<Router>
-    <Routes>
-        <Route path="" element={</>} />
-    </Routes>
-</Router>
-    );
+  return (
+    <Router>
+      <Header />
+      <Routes>
+        <Route path="/" element={<AddContact />} />
+        <Route path="/" element={<Home />} />
+        <Route path="/" element={<ShowUser />} />
+        <Route path="/" element={<SignIn />} />
+        <Route path="/" element={<SignUp />} />
+        <Route path="/" element={<UpdateUser />} />
+      </Routes>
+    </Router>
+  );
 }
 
 export default AppRouter;
