b7a7071febb2b6c04953f1015d32c6f1b6911937
Mendev200 <irca.mendev@gmail.com>
2024-04-14 18:15:13 +0200

    Displaying a list of contacts with options to update or delete, along with pagination and search functionality

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,8 +1,26 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import { Link } from "react-router-dom";
+import Delete from "../components/Delete";
+import { getContactList } from "../components/api/getContactList"; // Import de la fonction ContactListApi
+
 import '../css/pages/style.css';
 
 function Home() {
+    const [contacts, setContacts] = useState([]); // État pour stocker les contacts
+
+    useEffect(() => {
+        const fetchContacts = async () => {
+            try {
+                const contactsData = await getContactList(); // Utilisation de la fonction ContactListApi pour récupérer les contacts
+                setContacts(contactsData);
+            } catch (error) {
+                console.error(error);
+            }
+        };
+
+        fetchContacts(); // Appeler fetchContacts une fois que le composant est monté
+    }, []); // Utilisez une dépendance vide pour ne l'appeler qu'une seule fois
+
     return (
         <div>
             <div className="d-flex justify-content-between m-5">
@@ -12,22 +30,25 @@ function Home() {
                     <button className="btn btn-outline-success" type="submit">Search</button>
                 </form>
             </div>
-            <div className="d-flex flex-column align-items-center">
-                <img src={process.env.PUBLIC_URL + '/images/avatar.png'} alt="Logo" className="mb-4 rounded-circle" style={{ width: '150px' }} />
-                <h5 className="card-title fw-bold">Full Name</h5>
-                <p className="card-text">Professional Title</p>
-                <div className="card p-4 mt-3 mx-auto">
-                    <ul className="list-group list-group-flush mb-5">
-                        <li key="tel" className="card-text list-group-item my-3">Tel: 123-456-7890</li>
-                        <li key="email" className="card-text list-group-item my-3">Email: example@example.com</li>
-                        <li key="address" className="card-text list-group-item my-3">Address: 123 Street, City, Country</li>
-                    </ul>
-                    <div className="d-flex justify-content-end">
-                        <Link to="/update-user" className="btn btn-primary me-4">Update</Link>
-                        <Link className="btn btn-danger">Delete</Link>
+            {/* Affiche chaque contact dans la liste */}
+            {contacts.map((contact) => (
+                <div key={contact.id} className="d-flex flex-column align-items-center">
+                    <img src={contact.avatar} alt="Avatar" className="mb-4 rounded-circle" style={{ width: '150px' }} />
+                    <h5 className="card-title fw-bold">{contact.fullName}</h5>
+                    <p className="card-text">{contact.professionalTitle}</p>
+                    <div className="card p-4 mt-3 mx-auto">
+                        <ul className="list-group list-group-flush mb-5">
+                            <li key="tel" className="card-text list-group-item my-3">Tel: {contact.phoneNumber}</li>
+                            <li key="email" className="card-text list-group-item my-3">Email: {contact.email}</li>
+                            <li key="address" className="card-text list-group-item my-3">Address: {contact.address}</li>
+                        </ul>
+                        <div className="d-flex justify-content-end">
+                            <Link to={`/update-contact/${contact.id}`} className="btn btn-primary me-4">Update</Link>
+                            <Delete />
+                        </div>
                     </div>
                 </div>
-            </div>
+            ))}
             <div className="d-flex justify-content-end me-5">
                 <nav aria-label="Page navigation example ">
                     <ul className="pagination">
@@ -47,8 +68,6 @@ function Home() {
                     </ul>
                 </nav>
             </div>
-
-
         </div>
     );
 }
