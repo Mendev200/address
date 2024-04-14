03c20a0ff47bac86d7f2c67a45e40a322f85a228
Mendev200 <irca.mendev@gmail.com>
2024-04-14 18:15:40 +0200

    Adding a new contact with form submission and redirection upon successful addition

--- src/pages/AddContact.jsx
+++ src/pages/AddContact.jsx
@@ -1,8 +1,40 @@
-import React from "react";
+import React, { useState } from "react";
+import { Link, Navigate } from "react-router-dom"; // Import de Navigate depuis react-router-dom
+import { postAddContact } from "../components/api/postAddContact";
 import 'bootstrap/dist/css/bootstrap.min.css';
 import '../css/pages/style.css';
 
 function AddContact() {
+    const [formData, setFormData] = useState({
+        phoneNumber: "",
+        email: "",
+        address: "",
+        file: null
+    });
+    const [redirect, setRedirect] = useState(false); // État pour gérer la redirection après l'ajout réussi
+
+    const handleChange = (e) => {
+        if (e.target.type === "file") {
+            setFormData({ ...formData, file: e.target.files[0] });
+        } else {
+            setFormData({ ...formData, [e.target.name]: e.target.value });
+        }
+    };
+
+    const handleSubmit = async (e) => {
+        e.preventDefault();
+        try {
+            await postAddContact(formData);
+            setRedirect(true); // Déclenche la redirection après un ajout réussi
+        } catch (error) {
+            console.error(error);
+        }
+    };
+
+    if (redirect) {
+        return <Navigate to="/home" />; // Utilise Navigate pour rediriger vers "/home" après un ajout réussi
+    }
+
     return (
         <div className="container-fluid mt-5">
             <div className="row justify-content-center">
@@ -10,27 +42,27 @@ function AddContact() {
                     <div className="card mt-5 card-shadow card-pb-dix">
                         <div className="card-body d-flex flex-column justify-content-center">
                             <h1 className="fw-bold mb-4">Add contact</h1>
-                            <form>
+                            <form onSubmit={handleSubmit}>
                                 <div className="mb-3">
                                     <label htmlFor="phoneNumber" className="form-label fw-bolder">Phone number</label>
-                                    <input type="tel" placeholder="phone number" className="form-control custom-input w-100" id="phoneNumber" />
+                                    <input type="tel" placeholder="phone number" className="form-control custom-input w-100" id="phoneNumber" name="phoneNumber" value={formData.phoneNumber} onChange={handleChange} />
                                 </div>
                                 <div className="mb-3">
                                     <label htmlFor="email" className="form-label fw-bolder">Email</label>
-                                    <input type="email" placeholder="email" className="form-control custom-input" id="email" aria-describedby="" />
+                                    <input type="email" placeholder="email" className="form-control custom-input" id="email" name="email" value={formData.email} onChange={handleChange} />
                                 </div>
                                 <div className="mb-3">
-                                    <label htmlFor="adresse" className="form-label fw-bolder">Adresse</label>
-                                    <input type="text" placeholder="adresse" className="form-control custom-input mb-5" id="adresse" />
+                                    <label htmlFor="address" className="form-label fw-bolder">Address</label>
+                                    <input type="text" placeholder="address" className="form-control custom-input mb-5" id="address" name="address" value={formData.address} onChange={handleChange} />
                                 </div>
                                 <div className="mb-3">
                                     <div className="input-group">
-                                        <input type="file" className="form-control" id="fileInput" />
+                                        <input type="file" className="form-control" id="fileInput" name="file" onChange={handleChange} />
                                     </div>
                                 </div>
                                 <div className="d-flex justify-content-end">
-                                    <button className="btn" type="button">Cancel</button>
-                                    <button className="btn text-white btn-purple" type="button">Create</button>
+                                    <Link to="/home" className="btn">Cancel</Link>
+                                    <button className="btn text-white btn-purple" type="submit">Create</button>
                                 </div>
                             </form>
                         </div>
