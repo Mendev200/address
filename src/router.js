18bba538807bf5d9038eabddd1d716d5d0a310a4
Mendev200 <irca.mendev@gmail.com>
2024-04-14 09:24:01 +0200

    adding path of UpdateContact and path of Route

--- src/router.js
+++ src/router.js
@@ -6,23 +6,25 @@ import Home from './pages/Home';
 import ShowUser from './pages/ShowUser';
 import SignIn from './pages/SignIn';
 import SignUp from './pages/SignUp';
+import UpdateContact from './pages/UpdateContact';
 import UpdateUser from './pages/UpdateUser';
 
 
 function AppRouter() {
-  return (
-    <Router>
-      <Header />
-      <Routes>
-        <Route path="/" element={<AddContact />} />
-        <Route path="/" element={<Home />} />
-        <Route path="/" element={<ShowUser />} />
-        <Route path="/" element={<SignIn />} />
-        <Route path="/" element={<SignUp />} />
-        <Route path="/" element={<UpdateUser />} />
-      </Routes>
-    </Router>
-  );
+    return (
+        <Router>
+            <Header />
+            <Routes>
+                <Route path="/add-contact" element={<AddContact />} />
+                <Route path="/sign-in" element={<SignIn />} />
+                <Route path="/home" element={<Home />} />
+                <Route path="/show-user" element={<ShowUser />} />
+                <Route path="/sign-up" element={<SignUp />} />
+                <Route path="/update-contact" element={<UpdateContact />} />
+                <Route path="/update-user" element={<UpdateUser />} />
+            </Routes>
+        </Router>
+    );
 }
 
 export default AppRouter;
