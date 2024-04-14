1c76784c98cd40851143ef2826ab13e3f42d2a44
Mendev200 <irca.mendev@gmail.com>
2024-04-14 18:12:39 +0200

    Updating user information with form submission and redirection upon successful update

--- src/pages/UpdateUser.jsx
+++ src/pages/UpdateUser.jsx
@@ -1,9 +1,39 @@
-import React from "react";
+import React, { useState } from "react";
+import { Link, Navigate } from "react-router-dom"; // Import de Navigate depuis react-router-dom
+import { putUpdateUser } from "../components/api/putUpdateUser";
 import 'bootstrap/dist/css/bootstrap.min.css';
 import '../css/pages/style.css';
 
-function UpdateUser ()
-{
+function UpdateUser() {
+    const [formData, setFormData] = useState({
+        email: "",
+        password: "",
+        file: null
+    });
+    const [redirect, setRedirect] = useState(false); // État pour gérer la redirection après la mise à jour réussie
+
+    const handleChange = (e) => {
+        if (e.target.type === "file") {
+            setFormData({ ...formData, file: e.target.files[0] });
+        } else {
+            setFormData({ ...formData, [e.target.name]: e.target.value });
+        }
+    };
+
+    const handleSubmit = async (e) => {
+        e.preventDefault();
+        try {
+            await putUpdateUser(formData);
+            setRedirect(true); // Déclenche la redirection après une mise à jour réussie
+        } catch (error) {
+            console.error(error);
+        }
+    };
+
+    if (redirect) {
+        return <Navigate to="/show-user" />; // Utilise Navigate pour rediriger vers "/show_user" après une mise à jour réussie
+    }
+
     return (
         <div className="container-fluid mt-5">
             <div className="row justify-content-center">
@@ -11,23 +41,23 @@ function UpdateUser ()
                     <div className="card mt-5 card-shadow card-pb-dix">
                         <div className="card-body d-flex flex-column justify-content-center">
                             <h1 className="fw-bold mb-4">Update user</h1>
-                            <form>
+                            <form onSubmit={handleSubmit}>
                                 <div className="mb-3">
                                     <label htmlFor="email" className="form-label fw-bolder">Email</label>
-                                    <input type="email" placeholder="email@email.fr" className="form-control custom-input" id="email" aria-describedby="" />
+                                    <input type="email" placeholder="email@email.fr" className="form-control custom-input" id="email" name="email" value={formData.email} onChange={handleChange} />
                                 </div>
                                 <div className="mb-3">
                                     <label htmlFor="password" className="form-label fw-bolder">Password</label>
-                                    <input type="password" placeholder="Password" className="form-control custom-input mb-5" id="password" />
+                                    <input type="password" placeholder="Password" className="form-control custom-input mb-5" id="password" name="password" value={formData.password} onChange={handleChange} />
                                 </div>
                                 <div className="mb-3">
                                     <div className="input-group">
-                                        <input type="file" className="form-control" id="fileInput" />
+                                        <input type="file" className="form-control" id="fileInput" name="file" onChange={handleChange} />
                                     </div>
                                 </div>
                                 <div className="d-flex justify-content-end">
-                                    <button className="btn" type="button">Cancel</button>
-                                    <button className="btn text-white btn-purple" type="button">Create</button>
+                                    <Link to="/show-user" className="btn">Cancel</Link>
+                                    <button className="btn text-white btn-purple" type="submit">Update</button>
                                 </div>
                             </form>
                         </div>
