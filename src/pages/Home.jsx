f4f069605e1e8c6f8dc4a0d08d6f03f50a5c4e2d
Mendev200 <irca.mendev@gmail.com>
2024-04-16 16:05:55 +0200

    separation of view and logic

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,54 +1,21 @@
-import React, { useState, useEffect } from "react";
 import { Link } from "react-router-dom";
-import Delete from "../components/Delete";
-import { getContactList } from "../components/api/getContactList"; // Import de la fonction ContactListApi
-
+import ContactListCard from "../components/ContactListCard";
 import '../css/pages/style.css';
 
 function Home() {
-    const [contacts, setContacts] = useState([]); // État pour stocker les contacts
-
-    useEffect(() => {
-        const fetchContacts = async () => {
-            try {
-                const contactsData = await getContactList(); // Utilisation de la fonction ContactListApi pour récupérer les contacts
-                setContacts(contactsData);
-            } catch (error) {
-                console.error(error);
-            }
-        };
-
-        fetchContacts(); // Appeler fetchContacts une fois que le composant est monté
-    }, []); // Utilisez une dépendance vide pour ne l'appeler qu'une seule fois
 
     return (
         <div>
-            <div className="d-flex justify-content-between m-5">
+            <div className="d-flex justify-content-between m-3">
                 <Link to="/add-contact" className="btn-purple text-white text-decoration btn">Add new contact</Link>
                 <form className="d-flex" role="search">
                     <input className="form-control me-2" type="search" placeholder="Search..." aria-label="Search" />
                     <button className="btn btn-outline-success" type="submit">Search</button>
                 </form>
             </div>
-            {/* Affiche chaque contact dans la liste */}
-            {contacts.map((contact) => (
-                <div key={contact.id} className="d-flex flex-column align-items-center">
-                    <img src={contact.avatar} alt="Avatar" className="mb-4 rounded-circle" style={{ width: '150px' }} />
-                    <h5 className="card-title fw-bold">{contact.fullName}</h5>
-                    <p className="card-text">{contact.professionalTitle}</p>
-                    <div className="card p-4 mt-3 mx-auto">
-                        <ul className="list-group list-group-flush mb-5">
-                            <li key="tel" className="card-text list-group-item my-3">Tel: {contact.phoneNumber}</li>
-                            <li key="email" className="card-text list-group-item my-3">Email: {contact.email}</li>
-                            <li key="address" className="card-text list-group-item my-3">Address: {contact.address}</li>
-                        </ul>
-                        <div className="d-flex justify-content-end">
-                            <Link to={`/update-contact/${contact.id}`} className="btn btn-primary me-4">Update</Link>
-                            <Delete />
-                        </div>
-                    </div>
-                </div>
-            ))}
+            <div className="scroll-horizontal">
+                <ContactListCard />
+            </div>
             <div className="d-flex justify-content-end me-5">
                 <nav aria-label="Page navigation example ">
                     <ul className="pagination">
--- src/pages/UpdateContact.jsx
+++ src/pages/UpdateContact.jsx
@@ -1,40 +1,9 @@
-import React, { useState } from "react";
-import { Link, Navigate } from "react-router-dom"; // Import de Navigate depuis react-router-dom
-import { putUpdateContact } from "../components/api/putUpdateContact";
+import React from "react";
+import DataForm from "../components/DataForm";
 import 'bootstrap/dist/css/bootstrap.min.css';
 import '../css/pages/style.css';
 
 function UpdateContact() {
-    const [formData, setFormData] = useState({
-        phoneNumber: "",
-        email: "",
-        address: "",
-        file: null
-    });
-    const [redirect, setRedirect] = useState(false); // État pour gérer la redirection après la mise à jour réussie
-
-    const handleChange = (e) => {
-        if (e.target.type === "file") {
-            setFormData({ ...formData, file: e.target.files[0] });
-        } else {
-            setFormData({ ...formData, [e.target.name]: e.target.value });
-        }
-    };
-
-    const handleSubmit = async (e) => {
-        e.preventDefault();
-        try {
-            await putUpdateContact(formData);
-            setRedirect(true); // Déclenche la redirection après une mise à jour réussie
-        } catch (error) {
-            console.error(error);
-        }
-    };
-
-    if (redirect) {
-        return <Navigate to="/home" />; // Utilise Navigate pour rediriger vers "/home" après une mise à jour réussie
-    }
-
     return (
         <div className="container-fluid mt-5">
             <div className="row justify-content-center">
@@ -42,29 +11,7 @@ function UpdateContact() {
                     <div className="card mt-5 card-shadow card-pb-dix">
                         <div className="card-body d-flex flex-column justify-content-center">
                             <h1 className="fw-bold mb-4">Update contact</h1>
-                            <form onSubmit={handleSubmit}>
-                                <div className="mb-3">
-                                    <label htmlFor="phoneNumber" className="form-label fw-bolder">Phone number</label>
-                                    <input type="tel" placeholder="phone number" className="form-control custom-input w-100" id="phoneNumber" name="phoneNumber" value={formData.phoneNumber} onChange={handleChange} />
-                                </div>
-                                <div className="mb-3">
-                                    <label htmlFor="email" className="form-label fw-bolder">Email</label>
-                                    <input type="email" placeholder="email" className="form-control custom-input" id="email" name="email" value={formData.email} onChange={handleChange} />
-                                </div>
-                                <div className="mb-3">
-                                    <label htmlFor="address" className="form-label fw-bolder">Address</label>
-                                    <input type="text" placeholder="address" className="form-control custom-input mb-5" id="address" name="address" value={formData.address} onChange={handleChange} />
-                                </div>
-                                <div className="mb-3">
-                                    <div className="input-group">
-                                        <input type="file" className="form-control" id="fileInput" name="file" onChange={handleChange} />
-                                    </div>
-                                </div>
-                                <div className="d-flex justify-content-end">
-                                    <Link to="/home" className="btn">Cancel</Link>
-                                    <button className="btn text-white btn-purple" type="submit">Update</button>
-                                </div>
-                            </form>
+                            <DataForm />
                         </div>
                     </div>
                 </div>
