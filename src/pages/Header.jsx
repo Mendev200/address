fcf3ee28a43bbfe737dc7ca5ea521f120aa5a845
Mendev200 <irca.mendev@gmail.com>
2024-04-23 09:28:30 +0200

    adding UserId

--- src/pages/Header.jsx
+++ src/pages/Header.jsx
@@ -2,16 +2,18 @@ import React from "react";
 import { Link } from "react-router-dom";
 import 'bootstrap/dist/css/bootstrap.min.css';
 
-function Header() {
+function Header({ userId }) {
     return (
         <div>
             <nav className="navbar navbar-expand-lg navbar-light bg-dark">
-                <Link className="navbar-brand" to="/home">
+                <Link className="navbar-brand" to={`/home/${userId}`}>
                     <img src={process.env.PUBLIC_URL + '/images/logo.png'} alt="Logo" className="ms-3" />
                 </Link>
+                <Link to="/show-user">Show User</Link>
             </nav>
         </div>
     );
 }
 
 export default Header;
+
